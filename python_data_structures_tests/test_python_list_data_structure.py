f034a8dbb6cc08ba1eac8a297d4f707edaf1e119
reusmakrix <netporks@ya.ru>
2020-01-15 19:26:10 +0300

    Python list tests added.

--- python_data_structures_tests/test_python_list_data_structure.py
+++ python_data_structures_tests/test_python_list_data_structure.py
@@ -1,19 +1,42 @@
 def test_list_remove(get_list_fixture):
-    pass
+    l = get_list_fixture
+    l.remove(3)
+    result = True
+    for num in l:
+        if result:
+            result = (num == 3)
+    assert not result
 
 
 def test_list_append(get_list_fixture):
-    # print('>>>', get_list_fixture)
-    pass
+    l = get_list_fixture
+    l.append(11)
+    result = False
+    for num in l:
+        if not result:
+            result = (num == 11)
+    assert result
 
 
 def test_list_pop(get_list_fixture):
-    pass
+    l = get_list_fixture
+    l.pop(4)
+    result = True
+    for num in l:
+        if result:
+            result = (num == 5)
+    assert not result
 
 
 def test_list_4_extend(get_list_fixture):
-    pass
+    l1 = get_list_fixture
+    l2 = [11, 12, 13]
+    l1.extend(l2)
+    assert l1[7] == 13
 
 
-def test_list_5_insert_params(list_params_fixture):
-    pass
+def test_list_5_insert_params(get_list_fixture, list_params_fixture):
+    l1 = get_list_fixture
+    l2 = list_params_fixture
+    l1.insert(0, l2)
+    assert l1[0] == l2
